5522c44b1d3b0efa88d14ab66d4b0a6a59267fbb
Sourabh Marathe <surja795@gmail.com>
2024-02-03 13:21:36 -0500

    Ignore build files

---
+++ run_environment.py
@@ -0,0 +1,20 @@
+import os
+import json
+import logging
+import time
+import yaml
+
+from langchain.requests import Requests
+from langchain import OpenAI
+
+from utils import reduce_openapi_spec, ColorPrint
+from model import RestGPT
+
+logger = logging.getLogger()
+
+def main():
+    print("Hello world")
+
+
+if __name__ == '__main__':
+    main()
